e27e902ff4f58946cc13d382a7b295809ce20a84
Brecht Van der Elst <brechtvanderelst@gmail.com>
2024-03-30 16:49:32 +0100

    Add till page 30

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"interpreter/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s!\n", user.Username)
+
+	repl.Start(os.Stdin, os.Stdout)
+}
