b12f9b37384eb525e76fe6307fa1e87115c6d9f6
Brecht Van der Elst <brechtvanderelst@gmail.com>
2024-05-26 13:50:24 +0200

    p120

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -23,6 +23,9 @@ func Eval(node ast.Node) object.Object {
 		}
 	case *ast.Boolean:
 		return nativeBooleanToBooleanObject(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 
 	return nil
@@ -44,4 +47,26 @@ func nativeBooleanToBooleanObject(value bool) *object.Boolean {
 	}
 
 	return FALSE
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return FALSE
+	default:
+		return FALSE
+	}
 }
